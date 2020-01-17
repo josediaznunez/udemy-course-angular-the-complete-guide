4d1c3f21d45a169b98b48b0b754414e7402789d4
Jose Diaz-Nunez <josediaznunez3@gmail.com>
2020-01-16 23:13:26 -0600

    add components

--- ng-complete-guide/src/app/app.module.ts
+++ ng-complete-guide/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './header/shopping-list/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './header/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './header/recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './header/recipe-book/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './header/recipe-book/recipe-detail/recipe-detail.component';
+import { IngredientComponent } from './model/ingredient/ingredient.component';
+import { RecipeComponent } from './model/recipe/recipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    IngredientComponent,
+    RecipeComponent
   ],
   imports: [
     BrowserModule
