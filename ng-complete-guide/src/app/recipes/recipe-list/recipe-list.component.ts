416ae141eddde4fc350088a6a5f9fdd5cee942ea
Jose Diaz-Nunez <josediaznunez3@gmail.com>
2020-02-29 16:20:47 -0600

    manage recipes in recipe service

--- ng-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ng-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import { Recipe } from '../recipe.model';
+import {Recipe} from '../recipe.model';
+import {RecipeService} from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -11,15 +12,14 @@ export class RecipeListComponent implements OnInit {
   @Output()
   private recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe('Name 1', 'This is simply a test - 1', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'),
-    new Recipe('Name 2', 'This is simply a test - 2', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('Name 3', 'This is simply a test - 3', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) {
+  }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
+  }
 
   onRecipeSelected(recipe: Recipe) {
     this.recipeWasSelected.emit(recipe);
--- ng-complete-guide/src/app/recipes/recipes.component.ts
+++ ng-complete-guide/src/app/recipes/recipes.component.ts
@@ -1,18 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Recipe} from './recipe.model';
+import {RecipeService} from './recipe.service';
+import {UrlSerializer} from '@angular/router';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.css']
+  styleUrls: ['./recipes.component.css'],
+  providers: [RecipeService]
 })
 export class RecipesComponent implements OnInit {
 
   private selectedRecipe: Recipe;
 
-  constructor() { }
+  constructor() {
+  }
 
-  ngOnInit() { }
+  ngOnInit() {
+  }
 
   onRecipeClicked(recipeClicked: Recipe) {
     this.selectedRecipe = recipeClicked;
