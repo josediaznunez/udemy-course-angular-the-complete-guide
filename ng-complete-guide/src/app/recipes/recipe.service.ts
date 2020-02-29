3f48ed699d5716e5085a9920400500a1d3c06dc2
Jose Diaz-Nunez <josediaznunez3@gmail.com>
2020-02-29 16:21:24 -0600

    add recipe and shopping list services

---
+++ ng-complete-guide/src/app/recipes/recipe.service.ts
@@ -0,0 +1,17 @@
+import {Recipe} from './recipe.model';
+
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe('Name 1', 'This is simply a test - 1', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'),
+    new Recipe('Name 2', 'This is simply a test - 2', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe('Name 3', 'This is simply a test - 3', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+  ];
+
+  getRecipes(): Recipe[] {
+    // slice() returns new array/copy
+    return this.recipes.slice();
+  }
+
+  constructor() {
+  }
+}
