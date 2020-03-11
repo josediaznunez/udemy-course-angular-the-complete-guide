188a77183200a99d72eadf2a632226193e7be2f5
Jose Diaz-Nunez <josediaznunez3@gmail.com>
2020-03-11 18:30:17 -0500

    load server using id parameter - instructor solution

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -19,8 +19,11 @@ export class ServerComponent implements OnInit {
   }
 
   ngOnInit() {
-    const id = Number(this.route.snapshot.params['id']);
+    const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
+    this.route.params.subscribe((params: Params) =>
+      this.server = this.serversService.getServer(+params['id'])
+    );
   }
 
 }
