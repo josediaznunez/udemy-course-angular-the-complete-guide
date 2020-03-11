8c1a794ddd718af60a77c2b5da52d3ec24afd05d
Jose Diaz-Nunez <josediaznunez3@gmail.com>
2020-03-10 21:47:09 -0500

    pass parameters to routes

--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -6,9 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-  user: {id: number, name: string};
 
-  constructor() { }
+  user: { id: number, name: string };
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
