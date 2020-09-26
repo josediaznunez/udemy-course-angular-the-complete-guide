88e6eb815a1813edacdb7936a124c9b7853ff179
Jose Diaz-Nunez <josediaznunez3@gmail.com>
2020-09-26 14:08:22 -0500

    protect routes with canActivate; protect child, nested routes with canActivateChild

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { AuthGuard } from "./auth-guard.service";
 
 import { HomeComponent } from "./home/home.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
@@ -9,24 +10,30 @@ import { ServersComponent } from "./servers/servers.component";
 import { UserComponent } from "./users/user/user.component";
 import { UsersComponent } from "./users/users.component";
 
-const appRoutes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'users', component: UsersComponent, children: [
-        { path: ':id/:name',  component: UserComponent }
-    ] },
-    { path: 'servers', component: ServersComponent, children: [
-        { path: ':id', component: ServerComponent },
-        { path: ':id/edit', component: EditServerComponent }
-    ] },
-    { path: 'not-found', component: PageNotFoundComponent },
-    { path: '**', redirectTo: '/not-found' }
+const appRoutes: Routes = [ { 
+    path: '', component: HomeComponent 
+  }, {
+    path: 'users', component: UsersComponent, children: [
+      { path: ':id/:name', component: UserComponent }
+    ]
+  }, {
+    path: 
+      'servers', 
+    // canActivate: [AuthGuard], 
+    canActivateChild: [AuthGuard], 
+    component: ServersComponent, 
+    children: [
+      { path: ':id', component: ServerComponent },
+      { path: ':id/edit', component: EditServerComponent }
+    ]
+  }, 
+  { path: 'not-found', component: PageNotFoundComponent }, 
+  { path: '**', redirectTo: '/not-found' }
 ];
-  
+
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {
-    
-
 }
