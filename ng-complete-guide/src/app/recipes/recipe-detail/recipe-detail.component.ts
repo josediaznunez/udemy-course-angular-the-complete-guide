8d2b7fee8d5b8e1b44e860e9c7055cad18a476cc
Jose Diaz-Nunez <josediaznunez3@gmail.com>
2020-09-27 15:00:37 -0500

    add programmatic navigation to the edit page

--- ng-complete-guide/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ ng-complete-guide/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import {Recipe} from '../recipe.model';
 import {RecipeService} from '../recipe.service';
 
@@ -17,7 +17,8 @@ export class RecipeDetailComponent implements OnInit {
   isManageRecipeClicked = false;
 
   constructor(private recipeService: RecipeService, 
-              private route: ActivatedRoute) {
+              private route: ActivatedRoute, 
+              private router: Router) {
   }
 
   ngOnInit() {
@@ -30,6 +31,12 @@ export class RecipeDetailComponent implements OnInit {
   }
 
   onAddToShoppingList() {
-    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients)
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route}); // navigating withour ID, using relative path and current index in params
+    // this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route}); // more complicated navigation
+  }
+
 }
