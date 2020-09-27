9549dd16992f6e1513883094554cd46f939033fd
Jose Diaz-Nunez <josediaznunez3@gmail.com>
2020-09-27 14:34:40 -0500

    configure route parameters

--- ng-complete-guide/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ ng-complete-guide/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,5 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 import {Recipe} from '../recipe.model';
 import {RecipeService} from '../recipe.service';
 
@@ -8,15 +9,24 @@ import {RecipeService} from '../recipe.service';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input()
+  
   recipe: Recipe;
+
+  id: number;
 
   isManageRecipeClicked = false;
 
-  constructor(private recipeService: RecipeService) {
+  constructor(private recipeService: RecipeService, 
+              private route: ActivatedRoute) {
   }
 
   ngOnInit() {
+    this.route.params
+      .subscribe((params: Params) => {
+          this.id = +params['id'];
+          this.recipe = this.recipeService.getRecipe(this.id);
+        }
+      );
   }
 
   onAddToShoppingList() {
--- ng-complete-guide/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ ng-complete-guide/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,5 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Recipe} from '../../recipe.model';
-import {RecipeService} from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -12,13 +11,7 @@ export class RecipeItemComponent implements OnInit {
   @Input()
   private recipe: Recipe;
 
-  constructor(private recipeService: RecipeService) {
-  }
-
   ngOnInit() {
   }
 
-  onSelected() {
-    this.recipeService.recipeSelected.emit(this.recipe);
-  }
 }
