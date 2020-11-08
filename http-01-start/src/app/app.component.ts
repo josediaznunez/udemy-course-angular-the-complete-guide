56d31d288df1c555d76f18a0b12b4c00168a1726
Jose Diaz-Nunez <josediaznunez3@gmail.com>
2020-11-07 22:32:16 -0600

    sned a post request

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -15,7 +15,12 @@ export class AppComponent implements OnInit {
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    this.http.post(
+      'https://ng-complete-guide-330cf.firebaseio.com/posts.json', 
+      postData
+    ).subscribe(responseData => {
+      console.log(responseData);
+    });
   }
 
   onFetchPosts() {
