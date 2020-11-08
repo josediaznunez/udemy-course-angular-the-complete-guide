691f75009a3c5aba60ae2fd8f4f4997ccbaa35a6
Jose Diaz-Nunez <josediaznunez3@gmail.com>
2020-11-08 16:30:12 -0600

    change the response body type

--- http-01-start/src/app/post.service.ts
+++ http-01-start/src/app/post.service.ts
@@ -38,7 +38,8 @@ export class PostService {
       'https://ng-complete-guide-330cf.firebaseio.com/posts.json',
       {
         headers: new HttpHeaders({'Custom-Header': 'Hello'}),
-        params: searchParams
+        params: searchParams,
+        responseType: 'json'
       }
     )
     .pipe(
@@ -62,7 +63,8 @@ export class PostService {
     return this.http.delete(
       'https://ng-complete-guide-330cf.firebaseio.com/posts.json',
       {
-        observe: 'events'
+        observe: 'events',
+        responseType: 'text'
       }
     ).pipe(tap(event => {
       console.log(event);
